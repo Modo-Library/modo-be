70ee4f2ed39ea1a37dd5fcacfc4655bfd157f9e5
Donghwan Lee 84431962+hwanld@users.noreply.github.com <wkazxf@gmail.com>
2023-06-29 09:25:11 +0900

    [FEAT] Logging with Using AOP (NM-7)
    Debug controller, service and repository's each method.
    Info HttpRequest.
    Warn HttpRequest over 1000ms.

---
+++ src/main/java/modo/configuration/LoggingAspect.java
@@ -0,0 +1,84 @@
+package modo.configuration;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Log4j2
+@Component
+@Aspect
+public class LoggingAspect {
+    @Pointcut("execution(* modo.service..*(..))")
+    private void serviceTarget() {
+    }
+
+    @Pointcut("execution(* modo.repository..*(..))")
+    private void repositoryTarget() {
+    }
+
+    @Pointcut("execution(* modo.controller..*(..))")
+    private void controllerTarget() {
+    }
+
+    @Around("controllerTarget()")
+    public Object controllerExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String classpath = joinPoint.getSignature().getDeclaringType().getName();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            Signature signature = joinPoint.getSignature();
+            String className = joinPoint.getTarget().getClass().getSimpleName();
+            log.debug("{}.{}({}) : {}ms", classpath, signature.getName(), Arrays.toString(joinPoint.getArgs()), (finish - start));
+            log.info("{} {} {}ms", request.getMethod(), request.getRequestURI(), (finish - start));
+            if ((finish - start) > 1000) {
+                log.warn("Below Http Execution Time is Over 1000ms!");
+                log.warn("{} {} : {}ms", request.getMethod(), request.getRequestURI(), (finish - start));
+            }
+        }
+    }
+
+    @Around("serviceTarget()")
+    public Object serviceExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            Signature signature = joinPoint.getSignature();
+            String className = joinPoint.getTarget().getClass().getSimpleName();
+            log.debug("{}.{}({}) : {}ms", className, signature.getName(), Arrays.toString(joinPoint.getArgs()), (finish - start));
+        }
+    }
+
+    @Around("repositoryTarget()")
+    public Object dataAccessExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            Signature signature = joinPoint.getSignature();
+            String className = joinPoint.getTarget().getClass().getSimpleName();
+            log.debug("{}.{}({}) : {}ms", className, signature.getName(), Arrays.toString(joinPoint.getArgs()), (finish - start));
+        }
+    }
+
+}
