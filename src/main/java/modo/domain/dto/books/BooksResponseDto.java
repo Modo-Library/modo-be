d35ce34561d54d73d21e081f04a80ce4c6c01a46
Donghwan Lee 84431962+hwanld@users.noreply.github.com <wkazxf@gmail.com>
2023-07-23 21:45:09 +0900

    [FEAT] Books DTO : SaveRequest and Response (NM-60)

---
+++ src/main/java/modo/domain/dto/books/BooksResponseDto.java
@@ -0,0 +1,34 @@
+package modo.domain.dto.books;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import modo.domain.entity.Books;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class BooksResponseDto {
+    private Long booksId;
+    private String name;
+    private Long price;
+    private String status;
+    private String deadline;
+    private String description;
+    private String imgUrl;
+    private String createdAt;
+    private String modifiedAt;
+
+    @Builder
+    public BooksResponseDto(Books books) {
+        this.booksId = books.getBooksId();
+        this.name = books.getName();
+        this.price = books.getPrice();
+        this.status = books.getStatus().toString();
+        this.deadline = books.getDeadline().toString();
+        this.description = books.getDescription();
+        this.imgUrl = books.getImgUrl();
+        this.createdAt = books.getCreatedAt().toString();
+        this.modifiedAt = books.getModifiedAt().toString();
+    }
+}
---
+++ src/main/java/modo/domain/dto/books/BooksSaveRequestDto.java
@@ -0,0 +1,29 @@
+package modo.domain.dto.books;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import modo.domain.entity.Books;
+import modo.enums.BooksStatus;
+
+@AllArgsConstructor
+@Builder
+@Getter
+public class BooksSaveRequestDto {
+    private String name;
+    private Long price;
+    private String status;
+    private String description;
+    private String imgUrl;
+
+    public Books toEntity() {
+        return Books.builder()
+                .name(name)
+                .price(price)
+                .status(BooksStatus.valueOf(status))
+                .description(description)
+                .imgUrl(imgUrl)
+                .build();
+
+    }
+}
