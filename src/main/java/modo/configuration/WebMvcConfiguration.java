779fc607946df03c0ef0d1c32c6cebee6e2af851
Donghwan Lee 84431962+hwanld@users.noreply.github.com <wkazxf@gmail.com>
2023-07-25 11:36:58 +0900

    [FEAT] Spring Security : CORS Settings

--- src/main/java/modo/configuration/WebMvcConfiguration.java
+++ src/main/java/modo/configuration/WebMvcConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
