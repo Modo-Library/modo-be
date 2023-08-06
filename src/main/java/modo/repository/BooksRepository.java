a30bd74108e122e0fa4011118108a35648f20fe0
Donghwan Lee 84431962+hwanld@users.noreply.github.com <wkazxf@gmail.com>
2023-08-06 15:13:46 +0900

    [FEAT] Books Search : Pagination Repository SQL Function (NM-97)
    * findBooksByNameContainingWithDistanceWithPaging
    * findBooksWithDistanceWithPaging

--- src/main/java/modo/repository/BooksRepository.java
+++ src/main/java/modo/repository/BooksRepository.java
@@ -1,6 +1,8 @@
 package modo.repository;
 
 import modo.domain.entity.Books;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,7 +15,7 @@ public interface BooksRepository extends JpaRepository<Books, Long> {
     @Query(value = "SELECT b.books_id, b.name, b.price, b.status, b.deadline, b.description, b.img_url, b.created_at, b.modified_at, b.location, b.users_id, " +
             "ST_Distance_Sphere(point(:x,:y),b.location) AS diff_Distance " +
             "FROM books AS b " +
-            "HAVING diff_Distance >= :distance " +
+            "WHERE ST_Distance_Sphere(point(:x,:y),b.location) >= :distance " +
             "ORDER BY diff_Distance", nativeQuery = true)
     List<Books> findBooksWithDistance(double x, double y, double distance);
 
@@ -24,4 +26,20 @@ public interface BooksRepository extends JpaRepository<Books, Long> {
             "AND b.name LIKE CONCAT('%', :queryString, '%') " +
             "ORDER BY diff_Distance", nativeQuery = true)
     List<Books> findBooksByNameContainingWithDistance(double x, double y, double distance, String queryString);
+
+
+    @Query(value = "SELECT b.books_id, b.name, b.price, b.status, b.deadline, b.description, b.img_url, b.created_at, b.modified_at, b.location, b.users_id, " +
+            "ST_Distance_Sphere(point(:x,:y),b.location) AS diff_Distance " +
+            "FROM books AS b " +
+            "WHERE ST_Distance_Sphere(point(:x,:y),b.location) >= :distance " +
+            "ORDER BY diff_Distance", nativeQuery = true)
+    Page<Books> findBooksWithDistanceWithPaging(double x, double y, double distance, Pageable pageable);
+
+    @Query(value = "SELECT b.books_id, b.name, b.price, b.status, b.deadline, b.description, b.img_url, b.created_at, b.modified_at, b.location, b.users_id, " +
+            "ST_Distance_Sphere(point(:x,:y),b.location) AS diff_Distance " +
+            "FROM books AS b " +
+            "WHERE ST_Distance_Sphere(point(:x,:y),b.location) >= :distance " +
+            "AND b.name LIKE CONCAT('%', :queryString, '%') " +
+            "ORDER BY diff_Distance", nativeQuery = true)
+    Page<Books> findBooksByNameContainingWithDistanceWithPaging(double x, double y, double distance, String queryString, Pageable pageable);
 }
