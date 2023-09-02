710d09bcbd226748de10ed3cb8bb723288f5bcb7
Donghwan Lee 84431962+hwanld@users.noreply.github.com <wkazxf@gmail.com>
2023-09-02 19:20:53 +0900

    [CHORE] Clean-Up Test code (NM-126)
    * With deleting unused objects in test.

--- src/test/java/modo/socket/WebSocketServiceTest.java
+++ src/test/java/modo/socket/WebSocketServiceTest.java
@@ -3,16 +3,17 @@ package modo.socket;
 import lombok.extern.log4j.Log4j2;
 import modo.StaticResources;
 import modo.domain.dto.chat.ChatSendingMessages;
-import modo.repository.*;
+import modo.repository.BooksRepository;
+import modo.repository.ChatMessagesRepository;
+import modo.repository.ChatRoomsRepository;
+import modo.repository.UsersRepository;
 import modo.service.ChatService;
 import modo.service.WebSocketService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
@@ -49,15 +50,6 @@ public class WebSocketServiceTest {
 
     @Autowired
     UsersRepository usersRepository;
-
-    @Mock
-    SimpMessagingTemplate simpMessagingTemplate;
-
-    @Autowired
-    PicturesRepository picturesRepository;
-
-    @Autowired
-    UsersHistoryRepository usersHistoryRepository;
 
     @BeforeEach
     void saveTestSenderAndReceiverAndBook() {
